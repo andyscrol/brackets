a036f799f1f415926a82634e6de3c51d251c2191
Alexey Larin <alexey_larin@hotmail.com>
2023-02-14 11:39:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // Check if the input parameters are valid
+  if (typeof str !== 'string' || !Array.isArray(bracketsConfig)) {
+    // Throw an error if they are not
+    throw new Error('Invalid input parameters');
+  }
+  // Create a regular expression from the bracketsConfig array
+  let regex = new RegExp(bracketsConfig.map(pair => pair.join('').replace(/[{}()|[\]\\]/g, '\\$&')).join('|'), 'g');
+  // Replace the brackets pairs with an empty string using the regular expression until no more changes occur
+  while (str !== (str = str.replace(regex, '')));
+  // Return true if the string is empty, false otherwise
+  return str.length === 0;
 }
